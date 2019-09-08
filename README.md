c50e08ea508279f556f97ad1d8bb391817cd998c
Kevin Dunn <kgdunn@gmail.com>
2019-09-08 16:19:48 +0200

    Tutorial on setuptools

---
+++ README.md
@@ -0,0 +1,5 @@
+# Process Improvement (using Data)
+
+This is package that goes with the [online book](https://learnche.org/pid) of the 
+same title, Process Improvement using Data.
+
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="process-improve-kevindunn",
+    version="0.0.1",
+    author="Kevin Dunn",
+    author_email="kgdunn@gmail.com",
+    description="Process Improvement using Data, particularly Designed Experiments",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kgdunn/process_improve",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
