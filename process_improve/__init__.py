75f63b568e0b7c09f23e229753de8415c3c8c1a1
Kevin Dunn <Kevin.Dunn@dsm.com>
2019-10-09 14:55:42 +0200

    Updated API to expose some more internal functinos

--- process_improve/__init__.py
+++ process_improve/__init__.py
@@ -5,8 +5,9 @@ name = "process_improve"
 #__version__ = VERSION = version = pkg_resources.get_distribution('process_improve').version
 
 # Expose the API
-from . structures import (c, gather, expand_grid)
+from . structures import (c, gather, expand_grid, supplement)
 from . models import (lm, summary)
 from . plotting import (pareto_plot, contour_plot, predict_plot,
                        interaction_plot)
+from . designs_factorial import (full_factorial)
 
