2734c22d23c87aed8b94ede1038c5ce3b631094b
Kevin Dunn <Kevin.Dunn@dsm.com>
2019-11-20 12:38:32 +0100

    Expose the "predict" function in the API.

--- process_improve/__init__.py
+++ process_improve/__init__.py
@@ -10,7 +10,7 @@ name = "process_improve"
 # https://www.statsmodels.org/stable/importpaths.html
 
 from . structures import (c, gather, expand_grid, supplement)
-from . models import (lm, summary)
+from . models import (lm, summary, predict)
 from . plotting import (pareto_plot, contour_plot, plot_model,
                        interaction_plot, tradeoff_table)
 from . designs_factorial import (full_factorial)
