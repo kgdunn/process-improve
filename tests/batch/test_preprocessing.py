cd6869758d5fd0e8d6852c126cac868a58ffc9d9
Kevin Dunn <kgdunn@gmail.com>
2021-03-31 08:49:30 +0200

    All tests pass again. Coverage at 54%

--- tests/batch/test_preprocessing.py
+++ tests/batch/test_preprocessing.py
@@ -21,9 +21,7 @@ def dtw_understanding():
     plt.plot(x, reference, ".-", c="blue")
     plt.plot(x, query, ".-", c="red")
     plt.grid()
-    res = dtw(
-        query, y=reference, window_type="sakoechiba", window_size=int(0.2 * len(x))
-    )
+    res = dtw(query, y=reference, window_type="sakoechiba", window_size=int(0.2 * len(x)))
     warping_path = res.get_warping_path(target="query")
     plt.plot(x, query[warping_path], ".-", c="purple")
     plt.title("The query signal (red), aligned (purple) with the reference (blue)")
@@ -93,25 +91,19 @@ def test_scaling(dryer_data):
         "DryerTemp",
     ]
     scale_df = determine_scaling(
-        dryer_data,
-        columns_to_align=columns_to_align,
-        settings={"robust": False},
+        dryer_data, columns_to_align=columns_to_align, settings={"robust": False},
     )
     assert np.array([152.3796, 48.2545, 101.7032, 73.1462, 68.0041]) == approx(
         scale_df.loc[columns_to_align]["Range"]
     )
 
-    batches_scaled = apply_scaling(
-        dryer_data, scale_df, columns_to_align=columns_to_align
-    )
+    batches_scaled = apply_scaling(dryer_data, scale_df, columns_to_align=columns_to_align)
     reference_batch = batches_scaled["1"]
-    assert np.array([0.8354, 0.1660, 0.9833, 0.2556, 0.2884]) == approx(
+    assert np.array([0.793227, 0.171115, 1.007772, 0.051198, 0.050173]) == approx(
         reference_batch[columns_to_align].iloc[0], abs=1e-4
     )
     orig = reverse_scaling(batches_scaled, scale_df)
-    assert np.linalg.norm(orig["1"] - dryer_data["1"][columns_to_align]) == approx(
-        0, abs=1e-10
-    )
+    assert np.linalg.norm(orig["1"] - dryer_data["1"][columns_to_align]) == approx(0, abs=1e-10)
 
 
 def test_alignment(dryer_data):
@@ -126,10 +118,7 @@ def test_alignment(dryer_data):
         dryer_data,
         columns_to_align=columns_to_align,
         reference_batch="2",
-        settings={
-            "robust": False,
-            "tolerance": 1,
-        },  # high tolerance ensures only 1 iteration
+        settings={"robust": False, "tolerance": 1,},  # high tolerance ensures only 1 iteration
     )
     assert [1, 1, 1, 1, 1] == approx(outputs["weight_history"].iloc[0])
     assert [152.379618, 48.254502, 101.703155, 73.146169, 68.004085] == approx(
