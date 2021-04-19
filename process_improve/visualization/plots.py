fd0794c8a272aa49299f212e63dddf92a1b55572
Kevin Dunn <kgdunn@gmail.com>
2021-04-19 17:38:24 +0000

    Bump version and add more todos.

---
+++ process_improve/visualization/plots.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+
+def raincloud(sequence: pd.Series) -> dict:
+    """Creates a raincloud plot from a series of data
+
+    Parameters
+    ----------
+    sequence : pd.Series
+        A Pandas data series for which a raincloud plot will be created
+
+    Returns
+    -------
+    dict
+        A dictionary, which can be sent directly to Plotly for visualization.
+    """
+    pass
